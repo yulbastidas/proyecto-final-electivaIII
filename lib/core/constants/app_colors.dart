62c954846383a5db640c17d22fbd37ae822d8dce
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-10-29 22:13:49 -0500

    color

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF7E57C2);
+  static const secondary = Color(0xFFFFC107);
+  static const bg = Color(0xFFF7F7FA);
+  static const text = Color(0xFF2C3E50);
+}
